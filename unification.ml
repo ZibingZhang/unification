05b03f790fad5f72c0ced5dc0f1eab9b30b68e4f
Zibing Zhang <zibing.zha@gmail.com>
2021-04-01 01:34:32 -0400

    Unifies properly

--- unification.ml
+++ unification.ml
@@ -1,172 +1,221 @@
+;; (* ========== Resources ========== *)
 (* https://link.springer.com/chapter/10.1007/978-3-030-24986-1_48 *)
+(* https://dl.acm.org/doi/pdf/10.1145/357162.357169?casa_token=SEFEG_LBSOwAAAAA:eRfG5zFc7pJi7chayqwzuc3FtjeyJKbE5xbCp4mop7giKyHfpT-loehd8kyoLwKH5CAh-6dJMwCQ7A *)
 
-(**
- * IDEA : use a flag to detect cycles during creating of the substitution. 
- *)
+;; (* ========== TODO ========== *)
+(* remove tconst, use tcons with empty term list *)
+(* identical nodes should be the same node *)
 
-;; (* ========== Data Types ========== *)
+;; (* ========== Data Definitions ========== *)
 
 (**
  * Terms exist for ease of manually constructing data.
- * When unification occurs, all terms get translated into nodes.
- * TVars of the same name are represented by the same node.
+ * When unification occurs, all terms are translated into nodes.
  *)
+
 type term =
   | TConst of int
   | TVar of string
   | TCons of string * term list
 
 type node =
-  (* value * size * vars * class *)
-  | NConst of int * int ref * string list ref * node option ref
-  (* name * size * vars * class *)
-  | NVar of string * int ref * string list ref * node option ref
-  (* name * args * size * vars * class *)
-  | NCons of string * node list * int ref * string list ref * node option ref
-
-type subst = (string * node) list
+  (* value * class * visited *)
+  | NConst of int * cls option ref * bool ref
+  (* name * class * visited *)
+  | NVar of string * cls option ref * bool ref
+  (* name * args * class * visited *)
+  | NCons of string * node list * cls option ref * bool ref
+and cls =
+  (* rep * nodes * size * vars *)
+  | Class of node ref * node list ref * int ref * string list ref
 
 type 'a envt = (string * 'a) list
 
+type subst = node envt
+
+;; (* ========== Helper Functions ========== *)
+
+let get_cls_ref (s : node) : cls option ref =
+  match s with
+    | NConst(_, cls, _) -> cls
+    | NVar(_, cls, _) -> cls
+    | NCons(_, _, cls, _) -> cls
+
+let get_cls (s : node) : cls =
+  let maybe_cls = get_cls_ref s in
+    match !maybe_cls with
+      | None -> failwith "Impossible state."
+      | Some(c) -> c
+
+let set_class (s : node) (c : cls) : unit =
+  let c_ref = get_cls_ref s in
+    c_ref := Some c
+
+let has_visited (s : node) : bool =
+  let v = match s with
+    | NConst(_, _, v) -> v
+    | NVar(_, _, v) -> v
+    | NCons(_, _, _, v) -> v in 
+  !v
+
+let flag_visited (s : node) : unit =
+  let v = match s with
+    | NConst(_, _, v) -> v
+    | NVar(_, _, v) -> v
+    | NCons(_, _, _, v) -> v in 
+  v := true
+
+let unflag_visited (s : node) : unit =
+  let v = match s with
+    | NConst(_, _, v) -> v
+    | NVar(_, _, v) -> v
+    | NCons(_, _, _, v) -> v in
+  v := false
+
 ;; (* ========== to_string Functions ========== *)
 
-let rec node_to_string (s : node) =
+let rec node_to_string (s : node) : string =
   match s with
-    | NConst(n, _, _, _) -> Printf.sprintf "%d" n
-    | NVar(l, _, _, _) -> Printf.sprintf "%s" l
-    | NCons(s, l, _, _, _) ->
+    | NConst(n, _, _) -> Printf.sprintf "%d" n
+    | NVar(l, _, _) -> Printf.sprintf "%s" l
+    | NCons(s, l, _, _) ->
       Printf.sprintf "%s(%s)"
         s (String.concat ", " (List.map node_to_string l))
 
-let rec subst_to_string (o : subst) =
+let rec subst_to_string (o : subst) : string =
   match o with
-    | [] -> "\n"
+    | [] -> ""
     | (s, t)::r ->
       Printf.sprintf "%s -> %s\n%s"
         s (node_to_string t) (subst_to_string r)
 
-let rec node_envt_to_string (e : node envt) =
-  match e with
-    | [] -> "\n"
-    | (k, v)::r ->
-      Printf.sprintf "%s -> %s\n%s"
-        k (node_to_string v) (node_envt_to_string r)
-
 ;; (* ========== Environment Functions ========== *)
 
-let rec find (s : string) (env : 'a envt) =
+let rec find (s : string) (env : 'a envt) : 'a option =
   match env with
     | [] -> None
     | (k, v)::r ->
       if String.equal s k
-      then Some v
-      else find s r
+        then Some v
+        else find s r
+
+let contains (s : string) (env : 'a envt) : bool =
+  match find s env with
+    | None -> false
+    | Some(_) -> true
 
 ;; (* ========== Term->Node Setup Functions ========== *)
 
-let rec term_to_node (s : term) (env : node envt) =
+let rec term_to_node (s : term) (env : node envt) : node * node envt =
+  let rec map_over_args (l : term list) (env : node envt) (acc : node list): node list * node envt =
+    begin match l with
+      | [] -> List.rev acc, env
+      | a::r ->
+        let node, env = term_to_node a env in
+          map_over_args r env (node::acc)
+      end in
   let s_class = ref None in
-  let node, env = begin match s with
+  let node, vars, env = begin match s with
     | TVar(s) ->
         begin match find s env with
           | None ->
-            let node = NVar(s, (ref 1), (ref [s]), s_class) in
+            let node = NVar(s, s_class, ref false) in
             let env = (s, node)::env in 
-              node, env
+              node, (ref [s]), env
           | Some(node) ->
-              node, env
+              node, (ref [s]), env
         end
     | TConst(n) ->
-        NConst(n, (ref 1), (ref []), s_class), env
+        NConst(n, s_class, ref false), (ref []), env
     | TCons(s, l) -> 
-      let args, env = over_args l env [] in
-        NCons(s, args, (ref 1), (ref []), s_class), env
+      let args, env = map_over_args l env [] in
+        NCons(s, args, s_class, ref false), (ref []), env
   end in
-    s_class := Some node;
+    s_class := Some (Class ((ref node), (ref [node]), (ref 1), vars));
     node, env
 
-and over_args (l : term list) (env : node envt) (acc : node list) =
-  match l with
-    | [] -> List.rev acc, env
-    | a::r ->
-      let node, env = term_to_node a env in
-        over_args r env (node::acc)
-
 ;; (* ========== Unification Functions ========== *)
+
+let find_solution (s : node) : subst =
+  let rec add_vars (vars : string list) (s : node) (o : subst) : subst =
+    begin match vars with
+      | [] -> o
+      | a::r ->
+        if not (contains a o) && begin match s with
+          (* prevents x -> x *)
+          | NVar(s, _, _) -> s != a
+          | _ -> true
+        end
+          then (a, s)::(add_vars r s o)
+          else add_vars r s o
+    end in
+  let rec find_solution_args (l : node list) (o : subst) : subst =
+    begin match l with
+      | [] -> o
+      | a::r -> (find_solution_args r (find_solution_help a o))
+    end 
+  and find_solution_help (s : node) (o : subst) : subst =
+    if has_visited s then failwith "Cycle detected." else flag_visited s;
+    let s_class = get_cls s in
+    let Class(rep, _, _, vars) = s_class in
+    let t = !rep in
+    let o =
+      begin match t with
+        | NCons(_, args, _, _) -> find_solution_args args o
+        | _ -> o
+      end in
+    let result = add_vars !vars t o in
+      unflag_visited s;
+      result in
+  find_solution_help s []
 
 let rec unify (s : term) (t : term) : subst =
   let s_node, env = term_to_node s [] in
   let t_node, _ = term_to_node t env in
     unif_closure s_node t_node;
-    find_solution s_node []
+    find_solution s_node
 
-and unif_closure (s : node) (t : node) =
+and unif_closure (s : node) (t : node) : unit =
   (* not sure what is meant by the same node *)
   match s, t with
-    | NCons(n1, l1, _, _, _), NCons(n2, l2, _, _, _) ->
+    | NCons(n1, l1, _, _), NCons(n2, l2, _, _) ->
       if not (String.equal n1 n2)
-      then failwith "Cannot unify cons of different names."
-      else if not (List.length l1 = List.length l2)
-      then failwith "Unequal length of arguments."
-      else union s t; List.iter (fun (a1, a2) -> unif_closure a1 a2) (List.combine l1 l2)
-    (* constants must be the same *)
-    | NConst(n1, _, _, _), NConst(n2, _, _, _) ->
+        then failwith "Cannot unify cons of different names."
+        else if not (List.length l1 = List.length l2)
+          then failwith "Unequal length of arguments."
+          else union s t; List.iter (fun (a1, a2) -> unif_closure a1 a2) (List.combine l1 l2)
+    | NConst(n1, _, _), NConst(n2, _, _) ->
       if n1 != n2
-      then failwith "Constants clash."
-      else ()
+        then failwith "Constants clash."
+        else ()
     | NConst(_), NCons(_) -> failwith "Cannot unify constant with cons."
     | NCons(_), NConst(_) -> failwith "Cannot unify cons with constant."
     | _ -> union s t
 
-and union (s : node) (t : node) =
-  let s_size, s_vars, s_class = extract_mutable s in
-  let t_size, t_vars, t_class = extract_mutable t in
-  match s, t with
-    | NVar(_), _ ->
-      t_size := !t_size + !s_size;
-      t_vars := !t_vars @ !s_vars;
-      s_class := Some t
-    | _ ->
-      s_size := !s_size + !t_size;
-      s_vars := !s_vars @ !t_vars;
-      t_class := Some s
-
-and find_solution (s : node) (o : subst) =
-  let s_size, s_vars, s_class = extract_mutable s in
-  let t =
-    begin match !s_class with
-      | None -> failwith "Impossible state."
-      | Some t -> t
-    end in
-  let t_size, t_vars, t_class = extract_mutable t in
-  let o =
-    begin match t with
-      | NCons(_, args, _, _, _) ->
-        let rec find_solution_args (l : node list) (o : subst) =
-          begin match l with
-            | [] -> o
-            | a::r -> (find_solution_args r (find_solution a o))
-          end in
-          find_solution_args args o
-      | _ -> o
-    end in
-  let rec add_vars (vars : string list) (o : subst) =
-    begin match vars with
-      | [] -> o
+and union (s : node) (t : node) : unit =
+  let rec add_nodes (nodes : node list) (nodes_ref : node list ref) (c : cls) : unit =
+    begin match nodes with
+      | [] -> ()
       | a::r ->
-        if begin match t with
-          | NVar(s, _, _, _) -> s != a
-          | _ -> true
-        end then (a, t)::(add_vars r o)
-        else add_vars r o
-    end in
-      add_vars !t_vars o
-
-and extract_mutable (s : node) =
+        nodes_ref := a::(!nodes_ref);
+        set_class a c;
+        add_nodes r nodes_ref c
+    end in 
+  let combine_classes (s : cls) (t : cls) : unit =
+    let Class(_, s_nodes_ref, s_size_ref, s_vars_ref) = s in
+    let Class(_, t_nodes_ref, t_size_ref, t_vars_ref) = t in
+      t_size_ref := !t_size_ref + !s_size_ref;
+      t_vars_ref := !t_vars_ref @ !s_vars_ref;
+      add_nodes !s_nodes_ref t_nodes_ref t in
+  (* combine s into t *)
+  let combine (s : node) (t : node) : unit =
+    let s_class = get_cls s in
+    let t_class = get_cls t in
+      combine_classes s_class t_class;
+      set_class s (get_cls t) in
   match s with
-    | NConst(_, size, vars, cls) -> size, vars, cls
-    | NVar(_, size, vars, cls) -> size, vars, cls
-    | NCons(_, _, size, vars, cls) -> size, vars, cls
+    | NVar(_) -> combine s t
+    | _ -> combine t s
 
 ;; (* ========== Term Examples ========== *)
 
@@ -176,6 +225,9 @@ let t = TCons("f", [TVar("x")])
 let p = TCons("A", [TCons("B", [TVar("v"); TCons("C", [TVar("u"); TVar("v")])])])
 let q = TCons("A", [TCons("B", [TVar("w"); TCons("C", [TVar("w"); TCons("D", [TVar("x"); TVar("y")])])])])
 
+let a = TCons("f", [TVar("x"); TVar("x")])
+let b = TCons("f", [TVar("x"); TVar("x")])
+
 ;; (* ========== Unification Examples ========== *)
 
-print_string (subst_to_string (unify t t))
+print_string (subst_to_string (unify q p))
